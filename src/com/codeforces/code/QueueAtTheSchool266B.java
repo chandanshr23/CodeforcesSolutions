a54816aa37c8e23cb7c8508d8166343fde527ef1
chandan gowda <chandanshr8150@gmail.com>
2026-06-05 09:24:11 +0530

    266B

--- src/com/codeforces/code/QueueAtTheSchool266B.java
+++ src/com/codeforces/code/QueueAtTheSchool266B.java
@@ -1,6 +1,9 @@
-import java.util.*;
+package com.codeforces.code;
 
-public class Main {
+
+import java.util.Scanner;
+
+public class QueueAtTheSchool266B {
     public static void main(String[] args) {
     	  Scanner sc = new Scanner(System.in);
     	  int n=sc.nextInt();
