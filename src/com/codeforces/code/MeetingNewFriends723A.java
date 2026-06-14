823090f1636dffc451bf14d8b0067b7675a6062b
chandan gowda <chandanshr8150@gmail.com>
2026-06-14 22:16:38 +0530

    Meeting new friends

---
+++ src/com/codeforces/code/MeetingNewFriends723A.java
@@ -0,0 +1,19 @@
+package com.codeforces.code;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MeetingNewFriends723A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] x = new int[3];
+        for (int i = 0; i < 3; i++) {
+            x[i] = sc.nextInt();
+        }
+
+        Arrays.sort(x);
+
+        System.out.println(x[2] - x[0]);
+    }
+}
