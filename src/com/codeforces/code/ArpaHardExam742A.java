7fab60617d941b294c3952533f7d203a574d48c4
chandan gowda <chandanshr8150@gmail.com>
2026-06-23 03:46:10 +0530

    update

---
+++ src/com/codeforces/code/ArpaHardExam742A.java
@@ -0,0 +1,5 @@
+package com.codeforces.code;
+
+public class ArpaHardExam742A {
+
+}
--- src/com/codeforces/code/Leetcode1833MaxIceCream.java
+++ src/com/codeforces/code/Leetcode1833MaxIceCream.java
@@ -1,9 +1,17 @@
-package com.codeforces.code;
+z	package com.codeforces.code;
 
 public class Leetcode1833MaxIceCream {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int cnt=0;int sum=0;int n=costs.length;
+        Arrays.sort(costs);
+        int i=0;
+        while(sum<coins){
+            sum+=costs[i];
+            cnt++;
+            i++;
+        }
+        return 0;
 
 	}
 
