1166ab8526cf38b4c4369ff480a523ea4e4ecd71
chandan gowda <chandanshr8150@gmail.com>
2026-06-24 23:46:09 +0530

    SagheerCrossroads

--- src/com/codeforces/code/ArpaHardExam742A.java
+++ src/com/codeforces/code/ArpaHardExam742A.java
@@ -1,5 +1,20 @@
 package com.codeforces.code;
 
-public class ArpaHardExam742A {
+import java.util.*;
 
+public class ArpaHardExam742A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        if (n == 0) {
+            System.out.println(1);
+            return;
+        }
+
+        int[] ans = {6, 8, 4, 2};
+
+        System.out.println(ans[(int)(n % 4)]);
+    }
 }
---
+++ src/com/codeforces/code/SagheerCrossRoads812A.java
@@ -0,0 +1,5 @@
+package com.codeforces.code;
+
+public class SagheerCrossRoads812A {
+
+}
