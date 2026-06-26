b06686b7f28c5181ecd1ca83665024103184ac50
chandan gowda <chandanshr8150@gmail.com>
2026-06-26 23:23:43 +0530

    replacements

---
+++ src/com/codeforces/code/AdjacentReplcaement1006A.java
@@ -0,0 +1,5 @@
+package com.codeforces.code;
+
+public class AdjacentReplcaement1006A {
+
+}
--- src/com/codeforces/code/CheckTheStrings960A.java
+++ src/com/codeforces/code/CheckTheStrings960A.java
@@ -1,5 +1,40 @@
 package com.codeforces.code;
 
-public class CheckTheStrings960A {
+import java.util.*;
 
+public class CheckTheStrings960A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        int n = s.length();
+        int i = 0;
+
+        int a = 0, b = 0, c = 0;
+
+        while (i < n && s.charAt(i) == 'a') {
+            a++;
+            i++;
+        }
+
+        while (i < n && s.charAt(i) == 'b') {
+            b++;
+            i++;
+        }
+
+        while (i < n && s.charAt(i) == 'c') {
+            c++;
+            i++;
+        }
+
+        if (i != n || a == 0 || b == 0 || c == 0) {
+            System.out.println("NO");
+            return;
+        }
+
+        if (c == a || c == b)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
 }
