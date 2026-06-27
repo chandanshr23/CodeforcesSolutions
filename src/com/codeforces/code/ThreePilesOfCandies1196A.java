523b85d3e00ca6de3a92463ee14124db352a15fd
chandan gowda <chandanshr8150@gmail.com>
2026-06-28 00:31:33 +0530

    Three pile sof candies

--- src/com/codeforces/code/ThreePilesOfCandies1196A.java
+++ src/com/codeforces/code/ThreePilesOfCandies1196A.java
@@ -1,5 +1,19 @@
 package com.codeforces.code;
 
-public class ThreePilesOfCandies1196A {
+import java.util.*;
 
+public class ThreePilesOfCandies1196A {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+
+        int q = sc.nextInt();
+
+        while (q-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+
+            System.out.println((a + b + c) / 2);
+        }
+    }
 }
