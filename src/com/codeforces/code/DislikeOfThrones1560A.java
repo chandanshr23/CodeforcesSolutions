cb9daa348d38472b2b1b69972f714e9de5b7d392
chandan gowda <chandanshr8150@gmail.com>
2026-06-29 23:58:19 +0530

    Dislike The Thrones

---
+++ src/com/codeforces/code/DislikeOfThrones1560A.java
@@ -0,0 +1,35 @@
+package com.codeforces.code;
+import java.util.*;
+
+public class DislikeOfThrones1560A {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int k = sc.nextInt();
+
+            int count = 0;
+            int num = 1;
+
+            while (true) {
+
+                if (num % 3 != 0 && num % 10 != 3) {
+
+                    count++;
+
+                    if (count == k) {
+                        System.out.println(num);
+                        break;
+                    }
+                }
+
+                num++;
+            }
+        }
+    }
+}
