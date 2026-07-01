384404162eec6dce0ac128c6020304ea6ad89362
chandan gowda <chandanshr8150@gmail.com>
2026-07-01 23:42:04 +0530

    Sorabon

--- src/com/codeforces/code/FootBall96A.java
+++ src/com/codeforces/code/FootBall96A.java
@@ -1,5 +1,31 @@
 package com.codeforces.code;
 
+import java.util.*;
+
 public class FootBall96A {
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        int count = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count >= 7) {
+                System.out.println("YES");
+                return;
+            }
+        }
+
+        System.out.println("NO");
+    }
 }
---
+++ src/com/codeforces/code/Sorabon363A.java
@@ -0,0 +1,5 @@
+package com.codeforces.code;
+
+public class Sorabon363A {
+
+}
