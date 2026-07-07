29ba4784d00701c60cc90fb51f4ddbabbc8fb000
chandan gowda <chandanshr8150@gmail.com>
2026-07-07 07:33:26 +0530

    Digit Squence Dividing

---
+++ src/com/codeforces/code/DigitsSequenceDivinding1107A.java
@@ -0,0 +1,5 @@
+package com.codeforces.code;
+
+public class DigitsSequenceDivinding1107A {
+
+}
--- src/com/codeforces/code/PilesWithStones1013A.java
+++ src/com/codeforces/code/PilesWithStones1013A.java
@@ -1,5 +1,41 @@
 package com.codeforces.code;
 
-public class PilesWithStones1013A {
+import java.util.Scanner;
 
+public class PilesWithStones1013A {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        int[] x = new int[n];
+        int[] y = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            x[i] = scan.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            y[i] = scan.nextInt();
+        }
+
+        int need = 0;
+        int extra = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (y[i] > x[i]) {
+                need += y[i] - x[i];
+            } else {
+                extra += x[i] - y[i];
+            }
+        }
+
+        if (extra >= need) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        scan.close();
+    }
 }
