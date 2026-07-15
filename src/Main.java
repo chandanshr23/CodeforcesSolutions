71f89a0b32f2366b8d11476e73e861e28e13a8aa
chandan gowda <chandanshr8150@gmail.com>
2026-07-15 22:56:25 +0530

    MaximumGCD1370A

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,56 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        DataInputStream in = new DataInputStream(new BufferedInputStream(System.in, 1 << 16));
+        int t = nextInt(in);
+        StringBuilder sb = new StringBuilder();
 
-        int[] x = new int[3];
-        for (int i = 0; i < 3; i++) {
-            x[i] = sc.nextInt();
+        while (t-- > 0) {
+            int n = nextInt(in);
+            int m = nextInt(in);
+
+            long[] prefix = new long[n + 1];
+            for (int i = 1; i <= n; i++) {
+                long ai = nextInt(in);
+                prefix[i] = prefix[i - 1] + ai;
+            }
+
+            int[] b = new int[m];
+            for (int i = 0; i < m; i++) b[i] = nextInt(in);
+            Arrays.sort(b);
+
+            long ans = prefix[n] - prefix[b[m - 1]]; // fixed tail beyond max b
+            int prev = 0;
+            for (int i = 0; i < m; i++) {
+                int c = b[i];
+                long seg = prefix[c] - prefix[prev];
+                ans += Math.abs(seg);
+                prev = c;
+            }
+
+            sb.append(ans).append('\n');
         }
 
-        Arrays.sort(x);
+        System.out.print(sb);
+    }
 
-        System.out.println(x[2] - x[0]);
+    private static int nextInt(DataInputStream in) throws IOException {
+        int ret = 0;
+        int b = in.read();
+        boolean neg = false;
+        while (b != '-' && (b < '0' || b > '9')) {
+            b = in.read();
+        }
+        if (b == '-') {
+            neg = true;
+            b = in.read();
+        }
+        while (b >= '0' && b <= '9') {
+            ret = ret * 10 + (b - '0');
+            b = in.read();
+        }
+        return neg ? -ret : ret;
     }
 }
---
+++ src/com/codeforces/code/MaximumGCD1370A.java
@@ -0,0 +1,5 @@
+package com.codeforces.code;
+
+public class MaximumGCD1370A {
+
+}
