846f475df0d63826be2d337cbf6d37449a47ccb8
chandan gowda <chandanshr8150@gmail.com>
2026-08-12 21:57:43 +0530

    Domino Piling

---
+++ src/com/codeforces/code/DominoPiling50A.java
@@ -0,0 +1,13 @@
+package com.codeforces.code;
+import java.util.*;
+
+public class DominoPiling50A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+        System.out.println((M * N) / 2);
+    }
+}
